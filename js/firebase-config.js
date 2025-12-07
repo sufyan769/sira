0490e48e6d92cb1acdbe4a150c19451ff0326f75
sufyan769 <net.noon@gmail.com>
2025-12-07 16:00:13 +0000

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,4 +1,1 @@
-// اضبط هذا العنوان ليتوافق مع مسار Firebase Realtime Database لديك.
-// مثال: window.FIREBASE_DB_URL = 'https://your-app-default-rtdb.firebaseio.com/sirahData.json';
-// يمكن أيضاً إضافة معاملات auth في الرابط نفسه (مثل ?auth=TOKEN) بحسب القواعد لديك.
-window.FIREBASE_DB_URL = '';
+window.FIREBASE_DB_URL = 'https://sirahdata-default-rtdb.europe-west1.firebasedatabase.app/sirahData.json';
